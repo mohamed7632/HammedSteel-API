42f31f1141e0539a45e847d5fbf470f0f8e5d8c0
mohamed7632 <36171712+mohamed7632@users.noreply.github.com>
2020-06-15 12:18:41 +0200

    intiation

---
+++ app/Item.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $gaurded=['id'];
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+}
