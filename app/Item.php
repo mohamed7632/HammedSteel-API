21a57d6363aa99ff07d3572e73444a94f501e3af
mohamed7632 <36171712+mohamed7632@users.noreply.github.com>
2020-06-16 00:16:25 +0200

    table relations

--- app/Item.php
+++ app/Item.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Category;
+use App\Order;
 class Item extends Model
 {
     protected $gaurded=['id'];
     public function category(){
         return $this->belongsTo('App\Category');
+    }
+    public function order(){
+        return $this->belongsToMany('App\Order');
     }
 }
