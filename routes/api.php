85cccaae57e637801d8689776fe997fa8ef6aad0
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-06-17 13:48:03 +0200

    category controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('cat' , 'CategoryController@index');
+Route::get('cat/{id}' , 'CategoryController@show');
+Route::post('cat' , 'CategoryController@store');
+Route::put('cat/{id}' , 'CategoryController@update');
+Route::delete('cat/{id}' , 'CategoryController@delete');
