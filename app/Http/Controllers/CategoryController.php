ad02769e779692dad8727655e800e86996d7bc6a
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-06-19 16:40:19 +0200

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,33 +8,6 @@ use App\Category;
 
 class CategoryController extends Controller
 {
-    public function index(){
-        return Category::all();
-    }
-    public function show($id){
-        return Category::find($id);
-    }
-    public function store(Request $request){
-        $validatedData = $request->validate([
-            'name' => 'required|unique:categories',
-        ]);
-
-        if($validatedData){
-            echo "welcome";
-            $cat = new Category();   
-            $cat->name = $request->name;
-            $cat->save();   
-        }
-    }
-    public function update(Request $request , $id){
-          $cat = Category::
-                    where('id', $id)
-                    ->update($request->all());;
-    }
-    public function delete(Request $request,$id)
-    {
-        $cat  = Category::findOrFail($id);
-        $cat->delete();
-    }
+    
 
 }
