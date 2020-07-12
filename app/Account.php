896df9b43defe20bb910c354661696fc70facbb6
mohamed7632 <36171712+mohamed7632@users.noreply.github.com>
2020-07-12 15:10:24 +0200

    update

--- app/Account.php
+++ app/Account.php
@@ -7,12 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
   protected$fillable =['name','money','type'];
-  public function getMoneyAttribute()
-{
-    return "{$this->money} ";
-}
-public function setMoneyAttribute($value)
-{
-    $this->attributes['money'] = strtolower($value);
-}
-}
+  
